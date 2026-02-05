9249eb3b140bf13490832b6ef5f324317d110ce7
root <root@DESKTOP-9EGNJVN.localdomain>
2026-02-05 13:39:58 +0100

    mahdavi

---
+++ Assignment1/Task2/csharp/InsertionSort/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void InsertionSort(int[] arr)
+    {
+        for (int i = 1; i < arr.Length; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    static int[] WorstCaseArray(int n)
+    {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = n - i;
+        return arr;
+    }
+
+    static void Main()
+    {
+        int[] nValues = { 1000, 2000, 4000, 8000, 12000, 20000 };
+
+        foreach (int n in nValues)
+        {
+            int[] arr = WorstCaseArray(n);
+            Stopwatch sw = Stopwatch.StartNew();
+            InsertionSort(arr);
+            sw.Stop();
+            Console.WriteLine($"n={n}, time={sw.Elapsed.TotalSeconds:F4} s");
+        }
+    }
+}
